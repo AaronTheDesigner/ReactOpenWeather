af08f8bd5835b646916ed2df2c0afeb2eaa1c4a5
AaronTheDesigner <flux.charlotte@gmail.com>
2018-04-04 17:46:17 -0400

    error handler for unkown city

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,6 +2,9 @@ import {FETCH_WEATHER} from '../actions/index';
 
 
 export default function(state = [], action) {
+    if (action.error) {
+        return state;
+    }
     switch(action.type) {
         case FETCH_WEATHER:
             return state.concat([action.payload.data]);
